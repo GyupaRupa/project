8f0f949750e64a5344479beae25c66332ad1a17e
Gyuparupa <tuvisa96@gmail.com>
2020-11-09 12:42:07 +0900

    update repo name

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ db.app = app
 db.create_all()
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
-    #app.run(host='0.0.0.0', port=5000)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
